a5779901b67fb96458276312ac4c9d8f797088dc
Robert Ontiu <ontiu.robert@gmail.com>
2016-11-22 13:23:51 +0100

    fixing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require("./dist/wrapper6.min");
+export {default as queryObject} from "./lib/query-object";
+export {default as ReactiveMap} from "./lib/reactive-map";
+export {default as Module} from "./lib/module";
+export {default as Application} from "./lib/application";
+
+import bootstrap from "./lib/bootstrap";
+export default bootstrap;
--- src/wrapper6.js
+++
@@ -1,7 +0,0 @@
-export {default as queryObject} from "./query-object";
-export {default as ReactiveMap} from "./reactive-map";
-export {default as Module} from "./module";
-export {default as Application} from "./application";
-
-import bootstrap from "./bootstrap";
-export default bootstrap;
